b7d72fb0f9832f814000fdb48a12cad436104158
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2021-06-12 15:29:40 +0200

    Create colors-wal-dmenu.h

---
+++ wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cfdde3", "#08090B" },
+	[SchemeSel] = { "#cfdde3", "#4C7498" },
+	[SchemeOut] = { "#cfdde3", "#95B6C9" },
+};
